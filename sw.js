5d56d075bf9415eec953205ae58037dc9b9c3d2f
Mika <mika@roots.bj>
2026-08-19 08:58:37 +0100

    Configuration Evenementiel

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@
  */
 'use strict';
 
-var VERSION   = 'f597306542385d62';
+var VERSION   = '56e6eadca04d9e07';
 var COQUE     = 'roots-coque-'    + VERSION;
 var IMMUABLE  = 'roots-immuable-' + VERSION;
 var A_GARDER  = [COQUE, IMMUABLE];
