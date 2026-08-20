fbb0345d457d500847176fa27e4cf62edfcb0fab
Mika <mika@roots.bj>
2026-08-20 21:14:58 +0100

    Colors

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@
  */
 'use strict';
 
-var VERSION   = '845c88b7a713a3e4';
+var VERSION   = '39dff9d0d079fac8';
 var COQUE     = 'roots-coque-'    + VERSION;
 var IMMUABLE  = 'roots-immuable-' + VERSION;
 var A_GARDER  = [COQUE, IMMUABLE];
