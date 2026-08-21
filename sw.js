3fdac0a3e579f3caea122ec18c159c3c9a077ee1
Mika <mika@roots.bj>
2026-08-21 23:15:43 +0100

    Radio nav

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@
  */
 'use strict';
 
-var VERSION   = '75ad55967779d14a';
+var VERSION   = '707920eba840f105';
 var COQUE     = 'roots-coque-'    + VERSION;
 var IMMUABLE  = 'roots-immuable-' + VERSION;
 var A_GARDER  = [COQUE, IMMUABLE];
