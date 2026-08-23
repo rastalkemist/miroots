c5c1fa59396b4f1c5626ef59f9edf87b6c78f07d
Mika <mika@roots.bj>
2026-08-23 23:08:20 +0100

    Style

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@
  */
 'use strict';
 
-var VERSION   = 'd551758e48c6f8b4';
+var VERSION   = '9c85d3539baef441';
 var COQUE     = 'roots-coque-'    + VERSION;
 var IMMUABLE  = 'roots-immuable-' + VERSION;
 var A_GARDER  = [COQUE, IMMUABLE];
@@ -33,8 +33,9 @@ var REPLI = 'hors-ligne.html';
 var COQUE_FICHIERS = [
   'index.html', 'carte.html', 'retrouver.html', 'roam.html', 'plan.html',
   'facture.html', 'paiement.html', 'confidentialite.html', 'decouvrir-plan.html', REPLI,
-  'roots-fonts.css', 'roots-tokens.css', 'roots.css', 'plan.css',
-  'roots-db.js', 'roots.js', 'garde.js',
+  'roots-fonts.css', 'roots-tokens.css', 'roots.css', 'plan.css', 'roam.css',
+  'roots-db.js', 'roots.js', 'garde.js', 'roots-lecture.js',
+  'vendor/intlTelInput.min.css', 'vendor/intlTelInput.min.js', 'vendor/qrcode.js',
   'manifest.webmanifest', 'media/hero.jpg', 'media/hero-portrait.jpg'
 ];
 
