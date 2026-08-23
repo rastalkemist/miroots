e4c1cfa53adb3f7705e3690c35155cd9a390ea32
Mika <mika@roots.bj>
2026-08-23 10:51:00 +0100

    Roam 4

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@
  */
 'use strict';
 
-var VERSION   = '6fbb547ada5f8dd0';
+var VERSION   = '34d3d672b0264e96';
 var COQUE     = 'roots-coque-'    + VERSION;
 var IMMUABLE  = 'roots-immuable-' + VERSION;
 var A_GARDER  = [COQUE, IMMUABLE];
