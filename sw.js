8e3fcf137857ac1b8a912207ae17360d10e92fb7
Mika <mika@roots.bj>
2026-08-25 09:29:10 +0100

    Nav & Universe Colors

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@
  */
 'use strict';
 
-var VERSION   = '4c8c223044a016e5';
+var VERSION   = 'eda213fda08f8748';
 var COQUE     = 'roots-coque-'    + VERSION;
 var IMMUABLE  = 'roots-immuable-' + VERSION;
 var A_GARDER  = [COQUE, IMMUABLE];
