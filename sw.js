afb9d6730dc48b55d3011f4ce410ae4a5b7e33e1
Mika <mika@roots.bj>
2026-09-08 11:01:38 +0100

    Rail

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@
  */
 'use strict';
 
-var VERSION   = 'e373719dde795cdd';
+var VERSION   = '34bbb0bd25caa359';
 var COQUE     = 'roots-coque-'    + VERSION;
 var IMMUABLE  = 'roots-immuable-' + VERSION;
 var A_GARDER  = [COQUE, IMMUABLE];
